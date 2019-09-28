bef168c76de5b1924f55511d6b284cb26dce31ca
MilBia <milosz.bialczak@gmail.com>
2019-09-28 23:44:01 +0200

    add local_settings

--- TaleTalker/settings.py
+++ TaleTalker/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+try:
+    from TaleTalker.local_settings import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f(8v*h=l!+p=d_1)$!&b5i4h#@xxhde%0!*sij2q)7*)oynah-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+try:
+    from TaleTalker.local_settings import *
+except ImportError:
+    pass
